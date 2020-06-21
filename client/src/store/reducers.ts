437831c2e19456052bc69be1fd58941e33bbf734
Aaron Beasley <aaroncb@aaroncb.com>
2020-06-21 16:36:10 -0500

    ADD Redux && Began Adding Reducers
    - Added Redux to project for state management
    - Began adding needed reducers and types to project

---
+++ client/src/store/reducers.ts
@@ -0,0 +1,12 @@
+export const initialState = {
+  cell_size: 20,
+  grid_size: 800,
+  grid_rows: 800 / 20,
+  grid_cols: 800 / 20,
+  board: [],
+  cells: [],
+  offset: null,
+  rect: null,
+};
+
+
---
+++ client/src/store/types.ts
@@ -0,0 +1,8 @@
+export const SET_CELL_SIZE = "SET_CELL_SIZE";
+export const SET_GRID_SIZE = "SET_GRID_SIZE";
+export const SET_GRID_ROW = "SET_GRID_ROW";
+export const SET_GRID_COL = "SET_GRID_COL";
+export const SET_BOARD = "SET_BOARD";
+export const SET_CELLS = "SET_CELLS";
+export const SET_OFFSET = "SET_OFFSET";
+export const SET_RECT = "SET_RECT";
